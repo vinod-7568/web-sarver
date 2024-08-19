06f8d995eea0b0b6375cdcfc4cb64cb8d5409bd2
Hacker <bindasqueen995@gmail.com>
2024-08-19 17:09:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def send_message():
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ᴀʙʜɪ ʏᴀᴅᴀᴠ</title>
+  <title>ᴀʙʜɪ ʜᴏꜱᴛɪɴɢ 💫</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     body {
